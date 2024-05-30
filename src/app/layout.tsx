473e018c7a8b11105697b0f40657b85d678c0be2
Jan <96497313+3r08r@users.noreply.github.com>
2024-05-30 22:28:23 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Fabzact linktree",
   description: "Made By E_0r",
+  icons: "https://cdn.discordapp.com/avatars/257177177378914304/02066510896c8a92c949ca5c20673b65?size=1024",
 };
 
 export default function RootLayout({
