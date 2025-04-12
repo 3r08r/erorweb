4161c44a4009ffae8bb2d71394a1e8875a3fd3e4
Jan <jan.lasse.zander@outlook.de>
2025-04-12 14:23:12 +0200

    Refactor Next.js configuration and update package scripts for export and deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const isProd = process.env.NODE_ENV === 'production';
+export default {
+    output: 'export',
+    basePath: isProd ? '/your-repo-name' : '',
+    assetPrefix: isProd ? '/your-repo-name/' : '',
+  };
